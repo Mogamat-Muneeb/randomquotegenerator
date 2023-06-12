e8972ffb12870287bd1a30966bde031e46fe8b4c
Mogamat-Muneeb <davimuneeb785@gmail.com>
2023-06-12 19:26:20 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Quote from "./Quote";
 
-function App() {
+interface QuoteData {
+  quote: string;
+  author: string;
+}
+
+const App: React.FC = () => {
+  const [quoteData, setQuoteData] = useState<QuoteData | null>(null);
+  console.log("🚀 ~ file: App.tsx:110 ~ quoteData:", quoteData);
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
+  const fetchQuote = async () => {
+    try {
+      const category = "happiness";
+      const apiKey = "wJ5MC4koNhQ4CKKbFDg+Rg==MD2XxpRsx4oOdLoC"; 
+      const response = await axios.get(
+        `https://api.api-ninjas.com/v1/quotes?category=${category}`,
+        {
+          headers: { "X-Api-Key": apiKey },
+          /* @ts-ignore */
+          contentType: "application/json",
+        }
+      );
+      console.log(
+        "🚀 ~ file: App.tsx:128 ~ fetchQuote ~ response:",
+        response.data
+      );
+      const { quote, author } = response.data[0];
+      setQuoteData({ quote, author });
+    } catch (error) {
+      console.error("Error fetching quote:", error);
+    }
+  };
+
+  const handleNewQuote = () => {
+    fetchQuote();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="max-w-[1220px] mx-auto w-full">
+      <h1>Random Quote Generator</h1>
+      {quoteData ? (
+        <Quote quote={quoteData.quote} author={quoteData.author} />
+      ) : (
+        <p>Loading...</p>
+      )}
+      <button onClick={handleNewQuote}>New Quote</button>
     </div>
   );
-}
+};
 
 export default App;
